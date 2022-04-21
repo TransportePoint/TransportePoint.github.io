8b7017cdc3556c2e61eae28456c6d5fff007cab6
TransportePoint <104153036+TransportePoint@users.noreply.github.com>
2022-04-21 10:50:45 -0500

    footer, index, header

---
+++ index.php
@@ -0,0 +1,276 @@
+<?php 
+  include('header.html');
+  ?>
+<main>
+    <!--? slider Area Start-->
+    <div class="slider-area ">
+        <div class="slider-active">
+            <!-- Single Slider -->
+            <div class="single-slider slider-height d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-9 col-lg-9">
+                            <div class="hero__caption">
+                                <h1 >Point Asistencia al Comercio y Transporte</h1>
+                                </div>
+                            <!--Hero form
+                            <form action="#" class="search-box">
+                                <div class="input-form">
+                                    <input type="text" placeholder="Your Tracking ID">
+                                </div>
+                                <div class="search-form">
+                                    <a href="#">Track & Trace</a>
+                                </div>	
+                            </form>	
+                             Hero Pera 
+                            <div class="hero-pera">
+                                <p>For order status inquiry</p>
+                            </div>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider Area End-->
+    <!--? our info Start -->
+    <div class="our-info-area pt-70 pb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-info mb-30">
+                        <div class="info-icon">
+                            <span class="flaticon-support"></span>
+                        </div>
+                        <div class="info-caption">
+                            <p>Atención al cliente</p>
+                            <span>+52 (867) 158-73-56</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-info mb-30">
+                        <div class="info-icon">
+                            <span class="flaticon-clock"></span>
+                        </div>
+                        <div class="info-caption">
+                            <p>Horario</p>
+                            <span>Lunes - Sabado 8:00 am - 18:00 pm</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-info mb-30">
+                        <div class="info-icon">
+                            <span class="flaticon-place"></span>
+                        </div>
+                        <div class="info-caption">
+                            <p>Carretera Piedras Negras #1203</p>
+                            <span>Nuevo Laredo, Tamaulipas</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- our info End -->
+    <!--? Categories Area Start -->
+    <div class="categories-area section-padding20">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- Section Tittle -->
+                    <div class="section-tittle text-center mb-80">
+                        <span>Nuestros Servicios</span>
+                        <h2>Servicios a su medida</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-cat text-center mb-50">
+                        <div class="cat-icon">
+                            <span class="flaticon-ship"></span>
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="Pages/services.html">Documentacion al transporte</a></h5>
+                            <p><br>
+                            Los documentos de transporte no sólo facilitan la circulación de mercancías, también son, en ciertos casos, «los documentos de titularidad», es decir, el que tiene el documento tiene derecho a la posesión de la bienes.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-cat text-center mb-50">
+                        <div class="cat-icon">
+                            <span class="flaticon-shipped"></span>
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="Pages/services.html">Transporte</a></h5>
+                            <p>
+                            El transporte es un conjunto de procesos con la finalidad del desplazamiento y comunicación.​ Para poder llevar a cabo dichos procesos se emplean diferentes modos de transporte que circulan por determinados medios.
+                            <br>Nosotros ofrecemos servicio de calidad que nos distingue.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-cat text-center mb-50">
+                        <div class="cat-icon">
+                            <span class="flaticon-plane"></span>
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="Pages/services.html">Plataforma para maniobras</a></h5>
+                            <p>
+                            Las Plataformas Logísticas son zonas delimitadas creadas con el objetivo de realizar actividades logísticas, de transporte, distribución, etc. Son complejos preparados para centralizar operaciones logísticas, facilitando la optimización de las empresas que trabajen allí.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Categories Area End -->
+    <!--? About Area Start -->
+    <div class="about-low-area padding-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12">
+                    <div class="about-caption mb-50">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle mb-35">
+                            <span>Acerca de nosotros</span>
+                            <h2>Somos una empresa dedicada a la logistica y al transporte</h2>
+                        </div>
+                        <p>
+                            Nuestra empresa incluye toda la infraestructura que se necesita para realizar el movimiento de perosnas o productos a su destino.
+                        </p>
+                        <p>El transporte genera intercambios de todo tipo, hemos logrado que la transportacion  la comunicacion sean los canales principales para el desarrollo de las sociedades.</p>
+                        <a href="Pages/acerca_de.php" class="btn">Acerca de</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <!-- about-img -->
+                    <div class="about-img ">
+                        <div class="about-font-img">
+                            <img src="assets/img/gallery/about2.png" alt="">
+                        </div>
+                        <div class="about-back-img d-none d-lg-block">
+                            <img src="assets/img/gallery/about1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About Area End -->
+     <!--? Testimonial Start -->
+     <div class="testimonial-area testimonial-padding section-padding20" data-background="assets/img/gallery/section_bg04.jpg">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-xl-7 col-lg-7">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle section-tittle2 mb-25">
+                            <span></span>
+                            <h2> Mision, Vision y Valor.</h2>
+                        </div> 
+                        <div class="h1-testimonial-active mb-70">
+                            <!-- Single Testimonial -->
+                            <div class="single-testimonial ">
+                                <!-- Testimonial Content -->
+                                <div class="testimonial-caption ">
+
+                                  <!-- founder -->
+                                  <div class="testimonial-founder d-flex align-items-center">
+                                         <!--<div class="founder-img">
+                                            <img src="assets/img/gallery/Homepage_testi.png" alt="">
+                                        </div>-->
+                                        <div class="founder-text">
+                                            <span>MISION</span>
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="testimonial-top-cap">
+                                        <p>
+                                        Promover, desarrollar y explotar soluciones de movilidad, mediante la gestión y operación innovadora, eficiente y sostenible de servicios a la ciudadanía, al sector transportador, regional y privado, que impacten positivamente en la movilidad nacional y/o internacional.
+                                        </p>
+                                    </div>
+                                  
+                                </div>
+                            </div>
+                            <!-- Single Testimonial -->
+                            <div class="single-testimonial ">
+                                <!-- Testimonial Content -->
+                                <div class="testimonial-caption ">
+                                    <!-- founder -->
+                                    <div class="testimonial-founder d-flex align-items-center">
+                                            <!-- <div class="founder-img">
+                                            <img src="assets/img/gallery/Homepage_testi.png" alt="">
+                                        </div> -->
+                                        <div class="founder-text">
+                                            <span>VISION</span>
+                                        </div>
+                                    </div>
+                                
+                                    <div class="testimonial-top-cap">
+                                        <p>
+                                            Promover el mejor servicio de transporte de la ciudad, llegando a ser reconocido a nivel nacional e internacional por la calidad del servicio ofrecido a los clientes.
+                                        </p>
+                                    </div>
+                                   
+                                
+                                </div>
+                            </div>
+                             <!-- Single Testimonial -->
+                             <div class="single-testimonial ">
+                                <!-- Testimonial Content -->
+                                <div class="testimonial-caption ">
+                                    <!-- founder -->
+                                    <div class="testimonial-founder d-flex align-items-center">
+                                       <!--<div class="founder-img">
+                                            <img src="assets/img/gallery/Homepage_testi.png" alt="">
+                                        </div>-->
+                                        <div class="founder-text">
+                                            <span>VALOR</span>
+                                            <p></p>
+                                        </div>
+                                    </div>
+                                
+                                    <div class="testimonial-top-cap">
+                                        <p>
+                                            Compromiso con los clientes que se esmeran en la cotizacion de sus servicios, teniendo la responsabilidad de asumir las consecuencias derivado de la capacidad laboral de las trabajadores. 
+                                            Compartiendo conocimientos y experiencias con el esfuerzo de cada uno.
+                                        </p>
+                                    </div>
+                                   
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Form Start -->
+                    <div class="col-xl-4 col-lg-5 col-md-8">
+                        <div class="testimonial-form text-center">
+                            <h3>En constante actualizacion, para ofrecer el mejor servicio.</h3>
+                            <!-- <input type="text" placeholder="Incoterms">
+                            <button name="submit" class="submit-btn">Request a Quote</button>-->
+                        </div>
+                    </div>
+                    <!-- Form End -->
+                </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+
+        <!--division blanca-->
+    <br>
+    <br> 
+    <br>
+    <br>
+    <br>
+</main>
+<?php
+    include('footer.html');
+?>
